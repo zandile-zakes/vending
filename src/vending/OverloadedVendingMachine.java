5922d8ecd5a2e78fca2392aaf7fab9ac5bd7df6a
zandile-zakes <zbengeza14@gmail.com>
2020-05-20 14:25:55 +0200

    zandi

--- src/vending/OverloadedVendingMachine.java
+++ src/vending/OverloadedVendingMachine.java
@@ -1,4 +1,19 @@
 package vending;
-
+import vending.machine.SoftDrink;
+import vending.machine.SaltySnack;
+import vending.machine.Chocolate;
+import vending.machine.Product;
 public class OverloadedVendingMachine {
+    void buy(SoftDrink softdrink){
+        System.out.println("Buy softdrink");
+    }
+    void buy(SaltySnack saltySnack){
+        System.out.println("Buy saltySnack");
+    }
+    void buy(Chocolate chocolate){
+        System.out.println("Buy chocolate");
+    }
+    void buy(Product product){
+        System.out.println("Buy product");
+    }
 }
